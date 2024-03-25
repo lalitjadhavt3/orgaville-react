cda5ec7005305b7b6eca6a04d0ed80b2f1f2e807
lalitj003 <lalitj003@gmail.com>
2024-03-25 19:57:56 +0530

    cart data fetching with functionality

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,9 +1,17 @@
 import axios from 'axios';
-const ENV = 'TEST';
+export const ENV = 'PROD';
 export const BASE_URL =
-  ENV == 'PROD' ? 'https://admin.orgaville.com' : 'http://192.168.1.5:8000/'; // Replace with your API base URL
+  ENV == 'PROD' ? 'https://admin.orgaville.com' : 'http://192.168.1.5:8000'; // Replace with your API base URL
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 export default api;
+export const getImageUrl = imageName => {
+  const isProd = ENV === 'PROD';
+  // Base URL without /public
+  const baseUrl = isProd ? `${BASE_URL}/public` : BASE_URL;
+  // Append imageName to base URL
+  //console.log(`${baseUrl}${imageName}`);
+  return `${baseUrl}${imageName}`;
+};
